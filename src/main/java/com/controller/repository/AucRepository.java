7f089436d2d353307fbb0ddb3a8bfa6ad6775f81
Natia Baindurashvili <natia.baindurashvili.nb@gmail.com>
2024-05-15 00:12:10 +0200

    AUC

--- src/main/java/com/controller/repository/AucRepository.java
+++
@@ -1,7 +0,0 @@
-package com.controller.repository;
-
-import com.controller.entity.AucItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AucRepository extends JpaRepository<AucItem, Long> {
-}
